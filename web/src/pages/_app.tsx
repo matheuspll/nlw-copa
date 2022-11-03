1916085fd1840d6db02e36e84f3810be90e3ecb0
Matheus Magalhães <matheus.magalha95@gmail.com>
2022-11-02 21:57:46 -0300

    criando layout

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import type { AppProps } from 'next/app'
+import '../styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,72 @@
+// interface HomeProps {
+//   count: number
+// }
+
+import Image from 'next/image'
+import appPreviewImg from '../assets/app-nlw-copa-preview.png'
+import logoimg from '../assets/logo.svg'
+import usersAvatarExampleImg from '../assets/users-avatar-example.png'
+import iconCheckImg from '../assets/icon-check.svg'
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <Image src={logoimg} alt="NLW Copa" />
+        <h1>Crie seu próprio bolão da copa e compartilhe entre amigos!</h1>
+
+        <div>
+          <Image src={usersAvatarExampleImg} alt="" />
+        </div>
+
+        <strong>
+          <span>+12.592</span> pessoas já estão usando
+        </strong>
+
+        <form>
+          <input type="text" required placeholder="Qual nome do seu bolão" />
+          <button type="submit">Criar meu bolão</button>
+        </form>
+
+        <p>
+          Após criar seu bolão, você receberá um código único que poderá usar
+          para convidar outras pessoas 🚀
+        </p>
+
+        <div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+          <div>
+            <Image src={iconCheckImg} alt="" />
+            <div>
+              <span>+2.034</span>
+              <span>Bolões criados</span>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo uma prévia da aplicação móvel do NLW Copa"
+        quality={100}
+      />
+    </div>
+  )
+}
+
+// export const getServerSideProps = async () => {
+//   const response = await fetch('http://localhost:3333/pools/count')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       count: data.count,
+//     },
+//   }
+// }
